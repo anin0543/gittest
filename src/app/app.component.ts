449065beff3a97ccd909c21d59acc3d426467b9a
Anina Mahat <anin0543@edu.easj.dk>
2020-09-20 21:30:10 +0200

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import{User} from './user';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:`
+  
+  <app-user></app-user>`
+  
+  
+  
 })
+
+
 export class AppComponent {
   title = 'templatedrivenforms';
 }
